df1d64b9feeda254b5a8ae8d3aaa143a92fe70e0
HtooEainLwin <htooeainlwin12@gmail.com>
2022-12-28 14:54:21 +0630

    init shoulda matcha in rails helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -21,4 +21,6 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
 
   config.include Rails.application.routes.url_helpers
+  config.include(Shoulda::Matchers::ActiveModel, type: :model)
+  config.include(Shoulda::Matchers::ActiveRecord, type: :model)
 end
