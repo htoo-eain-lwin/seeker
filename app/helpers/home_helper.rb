8e13e4ec598df7ee52a1a503b7b78ffb68c2f88b
HtooEainLwin <htooeainlwin12@gmail.com>
2022-12-29 14:58:05 +0630

    added home index view

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,4 +1,19 @@
 # frozen_string_literal: true
 
 module HomeHelper
+  def resource_name
+    :user
+  end
+
+  def resource_class
+    User
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
 end
--- spec/helpers/home_helper_spec.rb
+++ spec/helpers/home_helper_spec.rb
@@ -2,16 +2,30 @@
 
 require 'rails_helper'
 
-# Specs in this file have access to a helper object that includes
-# the HomeHelper. For example:
-#
-# describe HomeHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
 RSpec.describe HomeHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#resource_name' do
+    it 'return user as a symbol' do
+      expect(resource_name).to eq(:user)
+    end
+  end
+
+  describe '#resource_class' do
+    it 'return user class' do
+      expect(resource_class).to eq(User)
+    end
+  end
+
+  describe '#resource' do
+    it 'return user object' do
+      user = User.new
+      allow(User).to receive(:new).and_return(user)
+      expect(resource).to eq(user)
+    end
+  end
+
+  describe '#devise_mapping' do
+    it 'return devise mapping' do
+      expect(devise_mapping).to eq(Devise.mappings[:user])
+    end
+  end
 end
