1bf2ba1638ec53466afd8fec20e3121450d16b90
HtooEainLwin <htooeainlwin12@gmail.com>
2023-01-01 06:01:55 +0630

    fix es-lint errors

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,8 +2,8 @@
 // Run that command whenever you add a new controller or create them with
 // ./bin/rails generate stimulus controllerName
 
-import { application } from "./application"
+import { application } from './application';
 
-import DropzoneController from "./dropzone_controller"
+import DropzoneController from './dropzone_controller';
 
-application.register("dropzone", DropzoneController)
+application.register('dropzone', DropzoneController);
