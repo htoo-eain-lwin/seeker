d092c1138006cae1e5bbce6f843310a6de0fe722
HtooEainLwin <htooeainlwin12@gmail.com>
2023-01-01 18:43:41 +0630

    remove home helper

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,19 +1,4 @@
 # frozen_string_literal: true
 
 module HomeHelper
-  def resource_name
-    :user
-  end
-
-  def resource_class
-    User
-  end
-
-  def resource
-    @resource ||= User.new
-  end
-
-  def devise_mapping
-    @devise_mapping ||= Devise.mappings[:user]
-  end
 end
