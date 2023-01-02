39013adf3b2f7c1b31eba316d8ce7a0d2c2e6cf4
HtooEainLwin <htooeainlwin12@gmail.com>
2023-01-02 23:18:04 +0630

    keyword controller spec update for unused methods

--- app/controllers/keywords_controller.rb
+++ app/controllers/keywords_controller.rb
@@ -3,19 +3,9 @@
 class KeywordsController < ApplicationController
   before_action :authenticate_user!
 
-  def create
-    @keyword = Keyword.find_or_create_by(name: keyword_params[:name])
-    if @keyword.save
-      redirect_to result_keyword_path(@keyword.name)
-    else
-      redirect_back_or_to dashboard_path
-    end
-  end
-
   def result
     keyword = Keyword.find_by name: params[:id]
-    CreateResultService.call(keyword)
-    @result = Result.find_by keyword_id: keyword
+    @result = keyword.result
   end
 
   private
