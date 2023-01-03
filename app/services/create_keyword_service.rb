5ddb103ad83b6252be8a2edf29961818f09419b9
HtooEainLwin <htooeainlwin12@gmail.com>
2023-01-03 14:15:17 +0630

    create keyword service and spec

---
+++ app/services/create_keyword_service.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+class CreateKeywordService < ApplicationService
+  def initialize(name, search_id)
+    @name = name
+    @keyword = Keyword.find_by(name: name)
+    @search = Search.find_by id: search_id
+    super
+  end
+
+  def call
+    return unless @search
+    return @keyword if @keyword
+
+    keyword = Keyword.create(name: @name)
+    @search.keywords << keyword
+    @search.user.keywords << keyword
+    keyword
+  end
+end
