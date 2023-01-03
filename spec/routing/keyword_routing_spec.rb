27c1e22c072743bc2a44d0982296247164f31afa
HtooEainLwin <htooeainlwin12@gmail.com>
2023-01-04 01:57:06 +0630

    fix failed spec and rubocop

--- spec/routing/keyword_routing_spec.rb
+++ spec/routing/keyword_routing_spec.rb
@@ -3,7 +3,6 @@
 require 'rails_helper'
 
 describe KeywordsController, type: :routing do
-  it { expect(get('/keywords')).to route_to('keywords#index') }
   it { expect(get('/keywords/foo')).to route_to('keywords#show', id: 'foo') }
   it { expect(get('/keywords/foo/result')).to route_to('keywords#result', id: 'foo') }
 end
