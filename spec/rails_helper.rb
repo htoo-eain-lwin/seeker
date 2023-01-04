caa90bc7bfb5514278e87a3e742f036ce0c59e82
HtooEainLwin <htooeainlwin12@gmail.com>
2023-01-04 22:50:26 +0630

    add view component dependency in rails helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -28,5 +28,4 @@ RSpec.configure do |config|
   config.include ViewComponent::TestHelpers, type: :component
   config.include ViewComponent::SystemTestHelpers, type: :component
   config.include Capybara::RSpecMatchers, type: :component
-
 end
