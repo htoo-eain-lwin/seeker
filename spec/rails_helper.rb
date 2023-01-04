9cbfe4628f3b0780eaa6c8d2ffb656ecaf6d01b3
HtooEainLwin <htooeainlwin12@gmail.com>
2023-01-04 22:06:46 +0630

    add flash component spec.rb

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -25,4 +25,8 @@ RSpec.configure do |config|
   config.include Devise::Test::IntegrationHelpers, type: :request
   config.include(Shoulda::Matchers::ActiveModel, type: :model)
   config.include(Shoulda::Matchers::ActiveRecord, type: :model)
+  config.include ViewComponent::TestHelpers, type: :component
+  config.include ViewComponent::SystemTestHelpers, type: :component
+  config.include Capybara::RSpecMatchers, type: :component
+
 end
