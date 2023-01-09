955c73c91cbb3bf0319eded686b55b4ebd69cd4c
HtooEainLwin <htooeainlwin12@gmail.com>
2023-01-09 16:26:56 +0630

    remove typo in db/migrate file

--- db/migrate/20230109062712_add_search_id_to_keywords.rb
+++ db/migrate/20230109062712_add_search_id_to_keywords.rb
@@ -3,27 +3,24 @@ class AddSearchIdToKeywords < ActiveRecord::Migration[7.0]
   def up
     add_reference :keywords, :search, foreign_key: true
     remove_index :keywords, :name
-    remove_foreign_key :search_keywords, :searches
-    remove_foreign_key :search_keywords, :keywords
-
     execute "INSERT INTO keywords(name, search_id, created_at, updated_at) " \
       "select keywords.name, search_keywords.search_id, NOW(), NOW() " \
       "From search_keywords " \
       "JOIN keywords ON search_keywords.keyword_id = keywords.id"
-    Keyword.where(search_id: nil).destroy_all
     remove_foreign_key :search_keywords, :searches
     remove_foreign_key :search_keywords, :keywords
+    Keyword.where(search_id: nil).destroy_all
     drop_table :search_keywords
   end
 
   def down
-    # create_table :search_keywords do |t|
-    #   t.belongs_to :search, index: true
-    #   t.belongs_to :keyword, index: true
-    #   t.timestamps
-    # end
+    create_table :search_keywords do |t|
+      t.belongs_to :search, index: true
+      t.belongs_to :keyword, index: true
+      t.timestamps
+    end
     execute "INSERT INTO search_keywords(search_id, keyword_id, created_at, updated_at) " \
-      "select id, search_id, created_at, updated_at " \
+      "select search_id, id, created_at, updated_at " \
       "From keywords"
     add_foreign_key :search_keywords, :searches
     add_foreign_key :search_keywords, :keywords
