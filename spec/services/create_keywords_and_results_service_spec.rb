77e65e8443a5d8987d9975d4ad591a1c151059c3
HtooEainLwin <htooeainlwin12@gmail.com>
2023-01-13 15:32:01 +0630

    update create keywords and results service with spec

--- spec/services/create_keywords_and_results_service_spec.rb
+++ spec/services/create_keywords_and_results_service_spec.rb
@@ -5,21 +5,15 @@ require 'rails_helper'
 RSpec.describe CreateKeywordsAndResultsService do
   include ActiveStorage::Blob::Analyzable
 
-  subject(:service) { described_class.new(search.id).call }
-
   let(:search) { Fabricate.create :search }
 
   describe 'call' do
-    it 'return if keywords are empty' do
-      allow(Search).to receive(:find_by).and_return(search)
-      allow(CsvService).to receive(:call).and_return([])
-      expect(service).to be_nil
-    end
+    it { expect(described_class.new(search.id, []).call).to be_nil }
 
     it 'create keywords' do
       allow(CreateResultsJob).to receive(:perform_async).and_return(true)
-      service
-      expect(Keyword.count).to eq(CsvService.call(search).count)
+      described_class.new(search.id, %w[foo bar]).call
+      expect(Keyword.count).to eq(2)
     end
   end
 end
