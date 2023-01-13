a46419e5ce4212aecc507edc936db9cdb4e49502
HtooEainLwin <htooeainlwin12@gmail.com>
2023-01-13 15:27:02 +0630

    remove active storage saving from search model

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,12 +1,8 @@
 # frozen_string_literal: true
 
 class Search < ApplicationRecord
-  MAX_SIZE = 1
-  has_one_attached :csv_file
   has_many :search_keywords, dependent: :destroy
   has_many :keywords, through: :search_keywords
 
   belongs_to :user
-
-  validates :csv_file, attached: true, content_type: :csv
 end
--- spec/models/search_spec.rb
+++ spec/models/search_spec.rb
@@ -11,8 +11,6 @@ RSpec.describe Search, type: :model do
 
   describe 'associations' do
     it { is_expected.to belong_to(:user) }
-    it { is_expected.to validate_attached_of(:csv_file) }
-    it { is_expected.to validate_content_type_of(:csv_file).allowing('.csv') }
     it { is_expected.to have_many(:keywords).through(:search_keywords) }
     it { is_expected.to have_many(:search_keywords).dependent(:destroy) }
   end
